1d97aa58e87f1f394953ac88df065cf19b616917
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2025-01-25 19:02:14 -0700

    adding app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
 import { useState, useEffect } from "react";
 function App() {
-  return <div className="flex items-center justify-center flex-col"></div>;
+  const [searchedCoin, setSearchedCoin] = useState("");
+  const [cryptoCoins, setCryptoCoins] = useState([]);
+
+  useEffect(() => {}, []);
+  return (
+    <div className="flex items-center justify-center flex-col gap-12 mt-8">
+      <h1 className="text-green-600 text-4xl">All Cryptocurrencies</h1>
+      <input
+        type="text"
+        className="outline-solid p-1 w-1/8 placeholder:text-xl placeholder:text-gray-700"
+        placeholder="Search..."
+        value={searchedCoin}
+        onChange={(e) => setSearchedCoin(e.target.value)}
+      />
+      <table>
+        <thead>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
 }
 
 export default App;
